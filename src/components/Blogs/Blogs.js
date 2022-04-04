1df2fe877c1752ce2a3f9e8090ddcfc030af399e
RafiUlI <ctgrafi150@gmail.com>
2022-04-04 22:36:30 +0600

    answer of questiion 1

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>blogs id de</h1>
+            <h3>what is context api:</h3>
+            <p>sohoj vasai bolte gele context api hlo props drilling er bikolpo rasta. tobe context api khuboi powerful jinis.ekta bihot aakarer web page e onek components ek tar vitore arekta hisebe thake. jodi ekhetre props drilling suru kori onek jhamela samne aase. jemon barbar destructuring , bar bar props pathano.esob somossa context api er maddome nimise ses kra jai. </p>
+
         </div>
     );
 };
