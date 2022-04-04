718c03f0e5aef7c7503a8fa96f2a80617e795a8d
RafiUlI <ctgrafi150@gmail.com>
2022-04-04 21:58:09 +0600

    dashBoard fixed

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import Chart from '../Chart/Chart';
+import './DashBoard.css';
 
 const DashBoard = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
     return (
-        <div>
-            <h1>dashboard is here</h1>
+        <div className='dash-board'>
+            <div>
+                <h3>Month Wise Sell</h3>
+                <LineChart
+                    width={500}
+                    height={400}
+                    data={products}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} /> */}
+                    <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+                </LineChart>
+            </div>
+            <div>
+                <h3>Investment Vs Reveneu</h3>
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={products}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                    <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                </AreaChart>
+            </div>
+
         </div>
     );
 };
