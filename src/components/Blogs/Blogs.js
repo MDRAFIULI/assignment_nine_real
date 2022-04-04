4df5d025ff30bf684603a5dc2309ffd45bd98a21
RafiUlI <ctgrafi150@gmail.com>
2022-04-04 22:37:26 +0600

    answer of questiion 2

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,6 +6,10 @@ const Blogs = () => {
             <h3>what is context api:</h3>
             <p>sohoj vasai bolte gele context api hlo props drilling er bikolpo rasta. tobe context api khuboi powerful jinis.ekta bihot aakarer web page e onek components ek tar vitore arekta hisebe thake. jodi ekhetre props drilling suru kori onek jhamela samne aase. jemon barbar destructuring , bar bar props pathano.esob somossa context api er maddome nimise ses kra jai. </p>
 
+            <h1>Answer of bonus number question:-</h1>
+            <h3>what is symantic tag?</h3>
+            <p>symantic tag hlo html 5 er ekti update version.html5 asar por html code likha onek gochalo hoyeche.sudhu gochalo noi performance valo krte-o symantic tag khuboi sohayok. search engine optimization e symantic tag khuboi meaningful hoi.koyek ti symantic tag hlo : header, nav, aside, main, section, footer, article, </p>
+
         </div>
     );
 };
